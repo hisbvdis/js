82c09e012808c24923a6a6c48971ddf631d79755
hisbvdis <hisbvdis@gmail.com>
2021-06-16 21:46:05 +0300

    'randomInt v.2' added; 'randomFloat' added

--- other/utils/number.js
+++ other/utils/number.js
@@ -1,22 +1,45 @@
 "use strict";
 
-
+// =============================================================================
 // Произвольное целое число из заданного диапазона
-// https://stackoverflow.com/q/1527803/4956416
-export function getRandomInt(min, max) {
+// =============================================================================
+// Вариант №1:
+// Источник: https://learn.javascript.ru/task/random-int-min-max
+export function randomInt(min, max) {
+  let randomFloat = min - 0.5 + Math.random() * (max - min + 1);
+  let randomInteger = Math.round(randomFloat);
+
+  return randomInteger;
+}
+
+
+// Вариант №2:
+// Источник: https://learn.javascript.ru/task/random-int-min-max
+function randomInt2(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
+
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 
+// =============================================================================
+// Произвольное дробное из заданного диапазона
+// =============================================================================
+function randomFloat(min, max) {
+	return Math.random() * (max - min) + min;
+}
+
+
+// =============================================================================
 // Произвольный массив чисел из заданного диапазона
+// =============================================================================
 function getRandomArray(min, max) {
-  let iterations = getRandomInt(min, max)
+  let iterations = randomInt(min, max)
   let resultSet = new Set();
 
   for (let i = 0; i < iterations; i++) {
-    resultSet.add( getRandomInt(min, max) );
+    resultSet.add( randomInt(min, max) );
   }
 
   return [...resultSet];
