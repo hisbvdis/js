5d66cbf3fa8594eb7b8f0349e3f2adb0bcd87176
hisbvdis <hisbvdis@gmail.com>
2021-06-16 21:20:20 +0300

    'getRandomInt' update

--- other/utils/number.js
+++ other/utils/number.js
@@ -2,21 +2,21 @@
 
 
 // Произвольное целое число из заданного диапазона
-export function randomFromRange(min, max) {
-  let randomFloat = min - 0.5 + Math.random() * (max - min + 1);
-  let randomInteger = Math.round(randomFloat);
-
-  return randomInteger;
+// https://stackoverflow.com/q/1527803/4956416
+export function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 
 // Произвольный массив чисел из заданного диапазона
-function randomArrayFromRange(min, max) {
-  let iterations = randomFromRange(min, max)
+function getRandomArray(min, max) {
+  let iterations = getRandomInt(min, max)
   let resultSet = new Set();
 
   for (let i = 0; i < iterations; i++) {
-    resultSet.add( randomFromRange(min, max) );
+    resultSet.add( getRandomInt(min, max) );
   }
 
   return [...resultSet];
