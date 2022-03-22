d03aaa494fb7a8d8772de986f8b7fde9114769a2
hisbvdis <hisbvdis@gmail.com>
2022-03-22 21:55:29 +0200

    Changes

--- recursion/7tree-average/script.js
+++ recursion/7tree-average/script.js
@@ -1,6 +1,9 @@
+// =============================================================================
+// Входящие данные
+// =============================================================================
 const tree = 
 {
-  values: [10, 11, 12],
+  values: [13, 14, 15],
   children: [
     {
       values: [1, 2, 3],
@@ -8,47 +11,51 @@ const tree =
     },
     {
       values: [4, 5, 6],
-      children: []
-    },
-    {
-      values: [7, 8, 9],
-      children: []
+      children: [
+        {
+          values: [7, 8, 9],
+          children: []
+        },
+        {
+          values: [10, 11, 12],
+          children: []
+        },
+      ]
     },
   ]
 }
 
-console.log( average(tree) );
 
 
-const average = graph => {
-  const values = inner(graph);
-  const avg = values.reduce((avg, value, index, array) => avg + value / array.length, 0);
-  return avg;
-
-  function inner(graph) {
+// =============================================================================
+// Рекурсивная функция
+// =============================================================================
+function averageValue(graph) {
+  // Рекурсивная функция сбора всех значений дерева
+  const collectValues = (graph) => {
     // 1. Простейший случай
     if (graph.children.length === 0) {
       return graph.values;
-    } 
-    // 2. Другие случаи
-    else {
-      // 2.1. Массив значений "values" текущего узла
-      const currentNodeValues = graph.values;
-
-      // 2.2. Массив значений "values" дочерних узлов
-      const childNodesValues = graph.children
-        .map(child => inner(child))
-        .filter(value => value !== undefined)
-        .reduce((accum, child) => [...accum, ...child], [])
-      // console.log( childNodesValues );
-
-      // 2.3. Массив значений текущего и дочерних узлов
-      const totalValues = [...currentNodeValues, ...childNodesValues]
-
-      // 2.4. Среднее арифметическое из итогового массива
-      // const avg = totalValues.reduce((avg, value, _, array) => avg + value / array.length, 0);
-
-      return totalValues;
     }
+
+    // 2. Сложный случай
+    const currentValues = graph.values;
+    const childValues = graph.children.reduce((accum, child) => accum.concat(collectValues(child)), []);
+    return [...currentValues, ...childValues];
   }
+
+
+  // Собрать все значения дерева
+  const allValues = collectValues(graph);
+
+  
+  // Вернуть среднее арифметическое из собранных значений
+  return allValues.reduce((avg, value, _, array) => avg + value / array.length, 0);
 }
+
+
+
+// =============================================================================
+// Вызов функции
+// =============================================================================
+console.log( averageValue(tree) );
